0952067e89720858cb6a3f85b576cf788227c01f
OrangeROT <fufumc135@163.com>
2026-05-18 15:41:42 +0800

    V2:Multi-turn CLI Conversation

--- main.py
+++ main.py
@@ -10,12 +10,38 @@ client = OpenAI(
     base_url="https://api.deepseek.com"
 )
 
-# 发起请求
-response = client.chat.completions.create(
-    model="deepseek-chat",
-    messages=[
-        {"role":"user", "content":"你好"}
-    ]
-)
+# 聊天记录
+messages = []
+# 循环，多轮对话
+while True:
+    # 获取用户输入
+    user_input =  input("你： ")
+    
+    if user_input == "exit":
+        break
+    
+    # 保存用户消息
+    messages.append(
+        {
+            "role": "user",
+            "content": user_input
+        }
+    )
+    
+    # 发送请求
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=messages
+    )
+    
+    ai_reply = response.choices[0].message.content
 
-print(response.choices[0].message.content)
+    print("AI:")
+    print(ai_reply)
+    
+    messages.append(
+        {
+            "role": "assistant",
+            "content": ai_reply
+        }
+    )
